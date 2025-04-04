7228210aa4f777e31ab6ea0425f706f491435529
Lawrence Wainwright <lawrencew02@gmail.com>
2025-04-04 15:45:54 +0100

    added home
    add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: UnderConstructionComponent }, // Default route
+  { path: '', component: HomeComponent }, // Default route
   { path: 'under-construction', component: UnderConstructionComponent },
 ];
